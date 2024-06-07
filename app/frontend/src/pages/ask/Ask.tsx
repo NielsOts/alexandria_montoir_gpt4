4238e827a8e9a7927d75e48cd4be73d9d7cf701d
Niels Ots <niels.ots@gmail.com>
2024-06-07 08:47:20 +0000

    edit example on chat line

--- app/frontend/src/pages/ask/Ask.tsx
+++ app/frontend/src/pages/ask/Ask.tsx
@@ -222,7 +222,7 @@ export function Component(): JSX.Element {
                 <h1 className={styles.askTitle}>Ask your data</h1>
                 <div className={styles.askQuestionInput}>
                     <QuestionInput
-                        placeholder="Example: Does my plan cover annual eye exams?"
+                        placeholder="Example: How is power generated?"
                         disabled={isLoading}
                         initQuestion={question}
                         onSend={question => makeApiRequest(question)}
